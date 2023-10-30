562ee1e01018263e74d656939af87303b2b7a371
TsukasaYa <115986372+TsukasaYa@users.noreply.github.com>
2023-10-30 20:47:13 +0900

    implement Chunkase

--- src/main/java/jp/ac/titech/c/se/diff/Chunkase.java
+++ src/main/java/jp/ac/titech/c/se/diff/Chunkase.java
@@ -1,6 +1,12 @@
 package jp.ac.titech.c.se.diff;
 
 import java.util.List;
+
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.eclipse.jgit.lib.CoreConfig.EOL;
+
+import ch.qos.logback.core.subst.Token.Type;
+
 import java.util.ArrayList;
 
 public class Chunkase {
@@ -13,20 +19,22 @@ public class Chunkase {
         int targetLine = 0;
 
         for (Chunk s : substrate) {
-            while(sourceLine < s.sourceStart && targetLine< s.targetStart){
+            // EQL in DP or AStar
+            if(s.type.equals(Chunk.Type.EQL)){
                 products.add(new Chunk(Chunk.Type.EQL, sourceLine, ++sourceLine, targetLine, ++targetLine));
             }
-
-            Chunk.Type type = switch (s.type) {
-                case DEL -> Chunk.Type.DEL;
-                case INS -> Chunk.Type.INS;
-                case MOD -> Chunk.Type.MOD;
-                default -> Chunk.Type.EQL;
-            };
-            
-            products.add(new Chunk(s.type, s.sourceStart, s.sourceEnd, s.targetStart, s.targetEnd));
-            sourceLine = s.sourceEnd;
-            targetLine = s.targetEnd;
+            // EQL in Hipster4J
+            while(sourceLine < s.sourceStart && targetLine < s.targetStart){
+                products.add(new Chunk(Chunk.Type.EQL, sourceLine, ++sourceLine, targetLine, ++targetLine));
+            }
+            // DEL
+            while(sourceLine < s.sourceEnd){
+                products.add(new Chunk(Chunk.Type.DEL, sourceLine, ++sourceLine, targetLine, targetLine));
+            }
+            // INS
+            while(targetLine < s.targetEnd){
+                products.add(new Chunk(Chunk.Type.INS, sourceLine, sourceLine, targetLine, ++targetLine));
+            }
         }
 
         return products;
